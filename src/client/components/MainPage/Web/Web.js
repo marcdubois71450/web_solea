b680c4d688e939b9a8d8b90f20a2c53b79378c30
Marc Dubois <marcdubois@MacBook-de-Marc.local>
2018-11-28 15:23:36 +0100

    test

--- src/client/components/MainPage/Web/Web.js
+++ src/client/components/MainPage/Web/Web.js
@@ -7,6 +7,7 @@ export default class Web extends Component {
     return (
       <div>
         <h1 className="MainPageTitle">Serveur Web</h1>
+        <a href="https://192.168.141.3/ui/#/console/6" target="_blank" onclick="window.open(this.href,_blank','toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=600px,height=600px');return false;">Popup link</a>
       </div>
     );
   }
