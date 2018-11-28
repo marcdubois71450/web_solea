3329d11935ece835b672de7c2961c4021c224e84
Marc Dubois <marcdubois@MacBook-de-Marc.local>
2018-11-28 10:50:10 +0100

    add login

--- src/client/components/LoginPage/LoginPage.js
+++ src/client/components/LoginPage/LoginPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './LoginPage.css';
 import { render } from 'react-dom';
 import Admin from './../Admin';
+import md5 from 'md5';
 
 
 
@@ -9,44 +10,35 @@ export default class LoginPage extends Component {
   constructor(props) {
         super(props);
         this.state = {
-            HashPassword: ''
+            HashPassword: ""
         };
         this.onChangePassword = this.onChangePassword.bind(this);
     }
 
 onChangePassword (event) {
    this.setState({ HashPassword: event.target.value });
-   var HashPassword = md5(md5(event.target.value));
  }
 
 
- static getDerivedStateFromProps(nextProps) {
-   if(nextProps.HashPassword) {
-     return {
-       HashPassword: nextProps.HashPassword,
-     };
-   }
-   return null;
- }
-
-  render() {
-    const { HashPassword } = this.state;
+ render() {
+    const HashPassword = this.state.HashPassword;
 
     return (
       <div>
-      {HashPassword =='c3284d0f94606de1fd2af172aba15bf3' ?
-                  <div className="LoginPage">
-                    <div>
-                      <form>
-                        <p className="TitleLogin">WebSoléa</p>
-                        <input value={this.state.HashPassword} onChange={this.onChangePassword} className="Form" placeholder="Mot de passe" type="password" name="password" /><br />
-                      </form>
-                    </div>
-                  </div>
+      {HashPassword == "admin" ?
+      <div>
+        <Admin />
+      </div>
       :
-                  <div>
-                    <Admin />
-                  </div>
+      <div className="LoginPage">
+        <div>
+          <form>
+            <p className="TitleLogin">WebSoléa</p>
+            <input value={this.state.HashPassword} onChange={this.onChangePassword} className="Form" placeholder="Mot de passe" type="password" name="password" /><br />
+          </form>
+        </div>
+      </div>
+
       }
       </div>
     );
